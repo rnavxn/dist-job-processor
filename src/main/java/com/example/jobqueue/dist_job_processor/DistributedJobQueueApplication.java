8074ab577cd69f60cdfdee8938bbbaa021534bf6
Navin <rnavxn@gmail.com>
2026-03-14 13:38:21 +0530

    Initial Commit

---
+++ src/main/java/com/example/jobqueue/dist_job_processor/DistributedJobQueueApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobqueue.dist_job_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedJobQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedJobQueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jobqueue/dist_job_processor/DistributedJobQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobqueue.dist_job_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedJobQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
