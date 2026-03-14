094d74f17594047741c1589b33c371087fdf47c7
Navin <rnavxn@gmail.com>
2026-03-14 17:28:18 +0530

    controller: Create JobController class to expose an endpoint

---
+++ src/main/java/com/example/jobqueue/dist_job_processor/controller/JobController.java
@@ -0,0 +1,31 @@
+package com.example.jobqueue.dist_job_processor.controller;
+
+import com.example.jobqueue.dist_job_processor.model.Job;
+import com.example.jobqueue.dist_job_processor.service.ProducerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/jobs")
+@RequiredArgsConstructor
+public class JobController {
+
+    private final ProducerService producerService;
+
+    public JobController(ProducerService producerService) {
+        this.producerService = producerService;
+    }
+
+    @PostMapping("/enqueue")
+    public String createJob(@RequestParam String type, @RequestParam String payload) {
+        // Create a new Job object
+        Job job = new Job();
+        job.setType(type);
+        job.setPayload(payload);
+
+        // Push it to Redis
+        producerService.enqueue(job);
+
+        return "Job " + job.getId() + " enqueued successfully!";
+    }
+}
